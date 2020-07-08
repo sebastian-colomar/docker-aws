d1d676c0b510ba771140a3d59a5ce2160bc28181
secobau <sebastian.colomar@gmail.com>
2020-07-09 01:57:28 +0200

    Update cluster-swarm-init.sh

--- bin/cluster-swarm-init.sh
+++ bin/cluster-swarm-init.sh
@@ -6,45 +6,19 @@
 #########################################################################
 set +x && test "$debug" = true && set -x				;
 #########################################################################
-test -n "$debug"	|| exit 100					;
-test -n "$stack"	|| exit 100					;
+token_worker="                                                          \
+  $(                                                                    \
+    sudo docker swarm init 2> /dev/null | grep token --max-count 1      \
+  )                                                                     \
+"                                                                       ;
+echo $token_worker                                                      ;
+echo RUN THIS TOKEN IN THE WORKERS                                      ;
 #########################################################################
-service=docker								;
-sleep=10								;
-#########################################################################
-targets=" InstanceMaster1 " 						;
-#########################################################################
-service_wait_targets $service $sleep $stack "$targets"			;
-#########################################################################
-command=" 								\
-  docker swarm init 2> /dev/null | grep token --max-count 1 		\
-" 									;
-token_worker="								\
-  $(									\
-    send_wait_targets "$command" $sleep $stack "$targets"		\
-  )									\
-"									;	
-#########################################################################
-command=" 								\
-  docker swarm join-token manager 2> /dev/null | grep token 		\
-" 									;
 token_manager="								\
   $(									\
-    send_wait_targets "$command" $sleep $stack "$targets"		\
+    sudo docker swarm join-token manager 2> /dev/null | grep token 		\
   )									\
-"									;	
-#########################################################################
-targets=" InstanceMaster2 InstanceMaster3 " 				;
-#########################################################################
-service_wait_targets $service $sleep $stack "$targets"			;
-#########################################################################
-command=" $token_manager 2> /dev/null " 				;
-send_wait_targets "$command" $sleep $stack "$targets"			;
-#########################################################################
-targets=" InstanceWorker1 InstanceWorker2 InstanceWorker3 " 		;
-#########################################################################
-service_wait_targets $service $sleep $stack "$targets"			;
-#########################################################################
-command=" $token_worker 2> /dev/null " 					;
-send_wait_targets "$command" $sleep $stack "$targets"			;
+"									;
+echo $token_manager                                                     ;
+echo RUN THIS TOKEN IN THE MASTERS                                      ;
 #########################################################################
