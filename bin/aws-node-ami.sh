f9dd2f8f3762e71da0706c205a63e1a128198b27
secobau <sebastian.colomar@gmail.com>
2020-07-09 01:44:25 +0200

    Update aws-node-ami.sh

--- bin/aws-node-ami.sh
+++ bin/aws-node-ami.sh
@@ -4,15 +4,15 @@
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
-set +x 	&& test "$debug" = true	&& set -x				;
+set +x 	&& test "$debug" = true	&& set -x                               ;
 #########################################################################
-yum update -y								;
-amazon-linux-extras install docker -y					;
-systemctl enable docker							;
-systemctl start docker							;
-while true								;
-do 									\
-  service docker status | grep running -q && break			;
-  sleep 10								;
-done									;
+sudo yum update -y                                                      ;
+sudo amazon-linux-extras install docker -y                              ;
+sudo systemctl enable docker                                            ;
+sudo systemctl start docker                                             ;
+while true                                                              ;
+do                                                                      \
+  sudo service docker status | grep running -q && break                 ;
+  sleep 10                                                              ;
+done                                                                    ;
 #########################################################################
