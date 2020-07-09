7cc8ac6ab73c5c51407eeee73b32be7b8a5a2001
secobau <sebastian.colomar@gmail.com>
2020-07-09 05:17:21 +0200

    Update cluster-kubernetes-worker.sh

--- bin/cluster-kubernetes-worker.sh
+++ bin/cluster-kubernetes-worker.sh
@@ -6,68 +6,55 @@
 #########################################################################
 set +x && test "$debug" = true && set -x				;
 #########################################################################
-test -n "$debug"                || exit 100                             ;
 test -n "$kube"    		|| exit 100                             ;
 test -n "$log"                	|| exit 100                             ;
 test -n "$token_discovery"      || exit 100                             ;
 test -n "$token_token"          || exit 100                             ;
 #########################################################################
-token_discovery="$(							\
-	echo								\
-		$token_discovery					\
-	|								\
-	base64								\
-		--decode						\
-)"							         	;
-token_token="$(								\
-	echo								\
-		$token_token						\
-	|								\
-	base64								\
-		--decode						\
-)"							         	;
+sudo sed --in-place                                                     \
+        /$kube/d                                                        \
+        /etc/hosts                                                      ;
+sudo sed --in-place                                                     \
+        /localhost4/s/$/' '$kube/                                       \
+        /etc/hosts                                                      ;
 #########################################################################
-compose=etc/docker/swarm/docker-compose.yaml				;
-uuid=$( uuidgen )							;
+token_discovery="$(                                                     \
+        echo                                                            \
+                $token_discovery                                        \
+        |                                                               \
+        base64                                                          \
+                --decode                                                \
+)"                                                                      ;
+token_token="$(                                                         \
+        echo                                                            \
+                $token_token                                            \
+        |                                                               \
+        base64                                                          \
+                --decode                                                \
+)"                                                                      ;
 #########################################################################
-git clone 								\
-	--single-branch --branch v1.1					\
-	https://github.com/secobau/nlb 					\
-	$uuid								;
-sed --in-place s/worker/manager/ $uuid/$compose				;
-sudo cp --recursive --verbose $uuid/run/* /run				;
-docker swarm init							;
-docker stack deploy --compose-file $uuid/$compose nlb			;
-while true								;
-do									\
-  sleep 1								;
-  docker service ls | grep '\([0-9]\)/\1' && break			;
-done									;
-sudo rm --recursive --force /run/secrets /run/configs			;
-sed --in-place 								\
-	/$kube/d 							\
-	/etc/hosts   		                                  	;
-sed --in-place 								\
-	/localhost4/s/$/' '$kube/ 					\
-	/etc/hosts          				             	;
-#########################################################################
-while true								;
-do									\
-        systemctl							\
-		is-enabled						\
-			kubelet                               		\
-	|								\
-	grep enabled    	                                      	\
-	&& break							\
+while true                                                              ;
+do                                                                      \
+        sudo systemctl                                                  \
+                is-enabled                                              \
+                        kubelet                                         \
+        |                                                               \
+                grep enabled                                            \
+                && break                                                \
                                                                         ;
-done									;	
+done                                                                    ;
 #########################################################################
-$token_token                                            		\
-	$token_discovery                                        	\
-	--ignore-preflight-errors					\
-		all							\
-	2>&1								\
-	|								\
-	tee $log							\
-									;
+while true                                                              ;
+do                                                                      \
+        sudo $token_token                                               \
+                $token_discovery                                        \
+                --ignore-preflight-errors                               \
+                        all                                             \
+                2>&1                                                    \
+        |                                                               \
+        tee $log                                                        \
+                                                                        ;
+        grep 'This node has joined the cluster' $log && break           ;
+        sleep 10                                                        ;
+done                                                                    ;
 #########################################################################
