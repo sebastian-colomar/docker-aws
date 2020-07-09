65f171a9fa0085c25981d8a32780e4aa2203bb3c
secobau <sebastian.colomar@gmail.com>
2020-07-09 04:14:49 +0200

    Update cluster-kubernetes-leader.sh

--- bin/cluster-kubernetes-leader.sh
+++ bin/cluster-kubernetes-leader.sh
@@ -4,79 +4,129 @@
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
-set +x && test "$debug" = true && set -x				;
+set +x && test "$debug" = true && set -x                                ;
 #########################################################################
 ip_leader=10.168.1.100                                                  ;
 kube=kube-apiserver                                                     ;
-log=/tmp/kubernetes-install.log                              		;
+log=/tmp/kubernetes-install.log                                         ;
 sleep=10                                                                ;
 #########################################################################
-calico=https://docs.projectcalico.org/v3.14/manifests			;
-cidr=192.168.0.0/16							;
-kubeconfig=/etc/kubernetes/admin.conf 					;
+calico=https://docs.projectcalico.org/v3.14/manifests                   ;
+cidr=192.168.0.0/16                                                     ;
+kubeconfig=/etc/kubernetes/admin.conf                                   ;
 #########################################################################
-while true								;
-do									\
-        sudo systemctl							\
-		is-enabled						\
-			kubelet                               		\
-	|								\
-		grep enabled                                          	\
-		&& break						\
+while true                                                              ;
+do                                                                      \
+        sudo systemctl                                                  \
+                is-enabled                                              \
+                        kubelet                                         \
+        |                                                               \
+                grep enabled                                            \
+                && break                                                \
                                                                         ;
-done									;
+done                                                                    ;
 #########################################################################
-echo $ip_leader $kube | sudo tee --append /etc/hosts                   	;
-sudo kubeadm init							\
-	--upload-certs							\
-	--control-plane-endpoint					\
-		"$kube"							\
-	--pod-network-cidr						\
-		$cidr							\
-	--ignore-preflight-errors					\
-		all							\
-	2>&1								\
-|									\
-tee --append $log							\
-									;
+echo $ip_leader $kube | sudo tee --append /etc/hosts                    ;
+sudo kubeadm init                                                       \
+        --upload-certs                                                  \
+        --control-plane-endpoint                                        \
+                "$kube"                                                 \
+        --pod-network-cidr                                              \
+                $cidr                                                   \
+        --ignore-preflight-errors                                       \
+                all                                                     \
+        2>&1                                                            \
+|                                                                       \
+tee --append $log                                                       \
+                                                                        ;
 #########################################################################
-kubectl apply								\
-	--filename							\
-		$calico/calico.yaml					\
-	--kubeconfig							\
-                $kubeconfig						\
-	2>&1								\
-|									\
-tee --append $log							\
-									;
+sudo kubectl apply                                                      \
+        --filename                                                      \
+                $calico/calico.yaml                                     \
+        --kubeconfig                                                    \
+                $kubeconfig                                             \
+        2>&1                                                            \
+|                                                                       \
+tee --append $log                                                       \
+                                                                        ;
 #########################################################################
-mkdir -p $HOME/.kube							;
-sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config			;
-sudo chown -R $(id -u):$(id -g) $HOME/.kube/config			;
-echo									\
-	'source <(kubectl completion bash)'				\
-|									\
-tee --append $HOME/.bashrc						\
-									;
+mkdir -p $HOME/.kube                                                    ;
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config                   ;
+sudo chown -R $(id -u):$(id -g) $HOME/.kube/config                      ;
+echo                                                                    \
+        'source <(kubectl completion bash)'                             \
+|                                                                       \
+tee --append $HOME/.bashrc                                              \
+                                                                        ;
 #########################################################################
-while true								;
-do									\
-	kubectl get node						\
-		--kubeconfig $kubeconfig				\
-	|								\
-	grep Ready							\
-	|								\
-	grep --invert-match NotReady					\
-	&&								\
-	break								\
-									;
-	sleep $sleep							;
-done									;
+while true                                                              ;
+do                                                                      \
+        kubectl get node                                                \
+        |                                                               \
+        grep Ready                                                      \
+        |                                                               \
+        grep --invert-match NotReady                                    \
+        &&                                                              \
+        break                                                           \
+                                                                        ;
+        sleep $sleep                                                    ;
+done                                                                    ;
 #########################################################################
-sudo sed --in-place 							\
-	/$kube/d 							\
-	/etc/hosts   		                                  	;
-sudo sed --in-place 							\
-	/localhost4/s/$/' '$kube/ 					\
-	/etc/hosts          				             	;
+sudo sed --in-place                                                     \
+        /$kube/d                                                        \
+        /etc/hosts                                                      ;
+sudo sed --in-place                                                     \
+        /localhost4/s/$/' '$kube/                                       \
+        /etc/hosts                                                      ;
+#########################################################################
+command="                                                               \
+        grep --max-count 1                                              \
+                certificate-key                                         \
+                $log                                                    \
+"                                                                       ;
+token_certificate=$(                                                    \
+        echo -n $command                                                \
+        |                                                               \
+        sed 's/\\/ /'                                                   \
+        |                                                               \
+        base64                                                          \
+                --wrap 0                                                \
+                                                                        ;
+)                                                                       ;
+#########################################################################
+command="                                                               \
+        grep --max-count 1                                              \
+                discovery-token-ca-cert-hash                            \
+                $log                                                    \
+"                                                                       ;
+token_discovery=$(                                                      \
+        echo -n $command                                                \
+        |                                                               \
+        sed 's/\\/ /'                                                   \
+        |                                                               \
+        base64                                                          \
+                --wrap 0                                                \
+                                                                        ;
+)                                                                       ;
+#########################################################################
+command="                                                               \
+        grep --max-count 1                                              \
+                kubeadm.*join                                           \
+                $log                                                    \
+"                                                                       ;
+token_token=$(                                                          \
+        echo -n $command                                                \
+        |                                                               \
+        sed 's/\\/ /'                                                   \
+        |                                                               \
+        base64                                                          \
+                --wrap 0                                                \
+                                                                        ;
+)                                                                       ;
+#########################################################################
+echo YOU WILL NEED THE FOLLOWING TOKENS TO COMPLETE THE INSTALL         ;
+echo FIRST IN THE MASTERS AND THEN IN THE WORKERS                       ;
+echo token_certificate=$token_certificate                               ;
+echo token_discovery=$token_discovery                                   ;
+echo token_token=$token_token                                           ;
 #########################################################################
