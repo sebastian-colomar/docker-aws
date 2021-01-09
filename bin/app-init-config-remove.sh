90fe106fb535d15ec83eff267a4574cae5815ac6
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-09 01:14:14 +0100

    Update app-init-config-remove.sh

--- bin/app-init-config-remove.sh
+++ bin/app-init-config-remove.sh
@@ -6,16 +6,16 @@
 #########################################################################
 set +x && test "$debug" = true && set -x				;
 #########################################################################
-test -n "$apps"                 && export apps           || exit 100    ;
-test -n "$A"	                && export A              || exit 100    ;
-test -n "$branch_docker_aws"    && export branch_docker_aws||exit 100   ;
-test -n "$branch_app"           && export branch_app     || exit 100    ;
-test -n "$debug"                && export debug          || exit 100    ;
-test -n "$domain"               && export domain         || exit 100    ;
-test -n "$mode"                 && export mode           || exit 100    ;
-test -n "$repository_app"       && export repository_app || exit 100    ;
-test -n "$stack"                && export stack          || exit 100    ;
-test -n "$username_app"         && export username_app   || exit 100    ;
+test -n "$apps"                 && export apps           || exit 901    ;
+test -n "$A"	                && export A              || exit 902    ;
+test -n "$branch_docker_aws"    && export branch_docker_aws||exit 903   ;
+test -n "$branch_app"           && export branch_app     || exit 904    ;
+test -n "$debug"                && export debug          || exit 905    ;
+test -n "$domain"               && export domain         || exit 906    ;
+test -n "$mode"                 && export mode           || exit 907    ;
+test -n "$repository_app"       && export repository_app || exit 908    ;
+test -n "$stack"                && export stack          || exit 909    ;
+test -n "$username_app"         && export username_app   || exit 910    ;
 #########################################################################
 file=common-functions.sh						;
 path=lib                                 				;
