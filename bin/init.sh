a3c420c6ebf138492c665ef9ef6bedd75ea0b759
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-09 01:06:09 +0100

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -6,26 +6,26 @@
 #########################################################################
 set +x && test "$debug" = true && set -x				;
 #########################################################################
-test -n "$A"	                && export A                 || exit 100 ;
-test -n "$apps" 		&& export apps	            || exit 100	;
-test -n "$branch_app" 	        && export branch_app	    || exit 100	;
-test -n "$branch_docker_aws"    && export branch_docker_aws || exit 100 ;
-test -n "$debug" 		&& export debug	            || exit 100	;
-test -n "$domain" 		&& export domain	    || exit 100	;
-test -n "$HostedZoneName"	&& export HostedZoneName    || exit 100 ;
-test -n "$Identifier"		&& export Identifier        || exit 100 ;
-test -n "$KeyName"              && export KeyName	    || exit 100	;
-test -n "$mode"                 && export mode	            || exit 100	;
-test -n "$repository_app"       && export repository_app    || exit 100	;
-test -n "$RecordSetName1"	&& export RecordSetName1    || exit 100 ;
-test -n "$RecordSetName2"	&& export RecordSetName2    || exit 100 ;
-test -n "$RecordSetName3"	&& export RecordSetName3    || exit 100 ;
-test -n "$RecordSetNameKube"	&& export RecordSetNameKube || exit 100 ;
-test -n "$stack"                && export stack	            || exit 100	;
-test -n "$template"		&& export template    	    || exit 100 ;
-test -n "$TypeMaster"		&& export TypeMaster 	    || exit 100 ;
-test -n "$TypeWorker"		&& export TypeWorker 	    || exit 100 ;
-test -n "$username_app"         && export username_app	    || exit 100	;
+test -n "$A"	                && export A                 || exit 101 ;
+test -n "$apps" 		&& export apps	            || exit 102	;
+test -n "$branch_app" 	        && export branch_app	    || exit 103	;
+test -n "$branch_docker_aws"    && export branch_docker_aws || exit 104 ;
+test -n "$debug" 		&& export debug	            || exit 105	;
+test -n "$domain" 		&& export domain	    || exit 106	;
+test -n "$HostedZoneName"	&& export HostedZoneName    || exit 107 ;
+test -n "$Identifier"		&& export Identifier        || exit 108 ;
+test -n "$KeyName"              && export KeyName	    || exit 109	;
+test -n "$mode"                 && export mode	            || exit 110	;
+test -n "$repository_app"       && export repository_app    || exit 111	;
+test -n "$RecordSetName1"	&& export RecordSetName1    || exit 112 ;
+test -n "$RecordSetName2"	&& export RecordSetName2    || exit 113 ;
+test -n "$RecordSetName3"	&& export RecordSetName3    || exit 114 ;
+test -n "$RecordSetNameKube"	&& export RecordSetNameKube || exit 115 ;
+test -n "$stack"                && export stack	            || exit 116	;
+test -n "$template"		&& export template    	    || exit 117 ;
+test -n "$TypeMaster"		&& export TypeMaster 	    || exit 118 ;
+test -n "$TypeWorker"		&& export TypeWorker 	    || exit 119 ;
+test -n "$username_app"         && export username_app	    || exit 120	;
 #########################################################################
 path=bin								;
 #########################################################################
